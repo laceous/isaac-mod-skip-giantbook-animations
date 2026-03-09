b598eca454c24159b18cbea924a188ed61f725ed
laceous <laceous@gmail.com>
2026-03-09 00:13:25 -0700

    add to github

---
+++ skip giantbook animations/main.lua
@@ -0,0 +1,50 @@
+local mod = RegisterMod('Skip Giantbook Animations', 1)
+
+if REPENTOGON then
+  function mod:onItemOverlayRender(giantbook)
+    -- SLEEP_NIGHTMARE can already be skipped, plus if you skip at the wrong time it can play twice
+    if giantbook == Giantbook.SLEEP_NIGHTMARE then -- DOGMA
+      return
+    end
+    
+    local sprite = ItemOverlay.GetSprite()
+    --print(giantbook, sprite:IsPlaying(), sprite:GetFilename(), sprite:GetAnimation(), sprite:GetFrame(), sprite.PlaybackSpeed)
+    if sprite:IsPlaying() then
+      if mod:isSkipTriggered() then
+        -- PlaybackSpeed seems to run any necessary triggers
+        -- unlike SetLastFrame which can skip triggers
+        local currentFrame = sprite:GetFrame()
+        sprite:SetLastFrame()
+        local lastFrame = sprite:GetFrame()
+        local diff = lastFrame - currentFrame
+        sprite:SetFrame(currentFrame)
+        if diff > 1 then
+          sprite.PlaybackSpeed = diff
+        end
+      end
+    else -- IsFinished
+      sprite.PlaybackSpeed = 1 -- reset
+    end
+  end
+  
+  -- ItemOverlay.GetPlayer doesn't seem to be reliable
+  function mod:isSkipTriggered()
+    local keyboard = 0
+    -- there's edge cases with space, just use enter for now
+    if Input.IsButtonTriggered(Keyboard.KEY_ENTER, keyboard) or Input.IsActionTriggered(ButtonAction.ACTION_DROP, keyboard) then
+      return true
+    end
+    
+    for _, player in ipairs(PlayerManager.GetPlayers()) do
+      if player.ControllerIndex > keyboard then
+        if Input.IsActionTriggered(ButtonAction.ACTION_MENUCONFIRM, player.ControllerIndex) or Input.IsActionTriggered(ButtonAction.ACTION_DROP, player.ControllerIndex) then
+          return true
+        end
+      end
+    end
+    
+    return false
+  end
+  
+  mod:AddCallback(ModCallbacks.MC_POST_ITEM_OVERLAY_RENDER, mod.onItemOverlayRender)
+end
